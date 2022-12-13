e75559a4ec7cf261f18dabaad0bb06e327b9f22e
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-13 14:49:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Interop.Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
