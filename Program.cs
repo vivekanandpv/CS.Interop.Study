eae1dec8d1f69e6fbba3dd71198fbc40dd5f8faf
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-13 15:44:52 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿//  In many ways the .NET can be considered as the successor to COM programming model
+//  COM implements the rudimentary garbage collection mechanism based on reference counting
+
+//  In COM, one always has to program for the interface. COM doesn't support method overloading.
+//  Optional and named parameters are allowed.
+
+//  We start by adding COM reference to the project
+
+using System;
+
+//  import the namespace
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace CS.Interop.Study
 {
@@ -6,14 +17,13 @@ namespace CS.Interop.Study
     {
         static void Main(string[] args)
         {
-            //  Interop means interoperations with either OS (Win32 API or Linux or Unix)
-            //  or COM (only on Windows)
+            var excelInstance = new Excel.Application();    //  an appropriate class will be used
+            excelInstance.Visible = true;
 
-            //  This is facilitated by pinvoke which allows us to access functions, structs,
-            //  and callbacks in unmanaged DLLs (or shared libraries in Unix)
-
-            //  For the list of API documentation, please refer to:
-            //  http://pinvoke.net/
+            Excel.Workbook workBook = excelInstance.Workbooks.Add();
+            ((Excel.Range)excelInstance.Cells[1, 1]).Font.FontStyle = "Bold";
+            ((Excel.Range)excelInstance.Cells[1, 1]).Value2 = "Hello, there!";
+            workBook.SaveAs(@"c:\users\vivek\com-demo.xlsx");
         }
     }
 }
