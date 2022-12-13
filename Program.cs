f88e80225e21cf0bc9c1e27c5c450aefaef2a871
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-13 14:52:40 +0530

    done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace CS.Interop.Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Interop means interoperations with either OS (Win32 API or Linux or Unix)
+            //  or COM (only on Windows)
+
+            //  This is facilitated by pinvoke which allows us to access functions, structs,
+            //  and callbacks in unmanaged DLLs (or shared libraries in Unix)
+
+            //  For the list of API documentation, please refer to:
+            //  http://pinvoke.net/
         }
     }
 }
